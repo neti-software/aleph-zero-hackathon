debe314c1fcd6d499ce262c47777d8428bfc2d38
Grzegorz Strączek <grzegorz.straczek@neti-soft.com>
2024-01-23 21:10:10 +0100

    Add metadata storage.Add only owner can set metadata.

--- contracts/src/phone_numbers/lib.rs
+++ contracts/src/phone_numbers/lib.rs
@@ -1,9 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-#[openbrush::implementation(PSP34, AccessControl)]
+#[openbrush::implementation(PSP34, AccessControl, PSP34Metadata, Ownable)]
 #[openbrush::contract]
 pub mod phone_numbers {
-    use openbrush::{modifiers, traits::Storage};
+    use openbrush::{contracts::ownable, modifiers, traits::Storage};
 
     #[ink(storage)]
     #[derive(Default, Storage)]
@@ -12,6 +12,10 @@ pub mod phone_numbers {
         psp34: psp34::Data,
         #[storage_field]
         access: access_control::Data,
+        #[storage_field]
+        metadata: metadata::Data,
+        #[storage_field]
+        ownable: ownable::Data,
     }
 
     const CENTRAL_AUTHORITY: RoleType = ink::selector_id!("CENTRAL_AUTHORITY");
@@ -24,13 +28,30 @@ pub mod phone_numbers {
             access_control::Internal::_init_with_admin(&mut instance, Some(caller));
             AccessControl::grant_role(&mut instance, CENTRAL_AUTHORITY, Some(caller))
                 .expect("Should grant CENTRAL_AUTHORITY role");
+            ownable::Internal::_init_with_owner(&mut instance, caller);
             instance
         }
 
         #[ink(message)]
         #[modifiers(only_role(CENTRAL_AUTHORITY))]
         pub fn mint(&mut self, phone_number: Vec<u8>) -> Result<(), PSP34Error> {
-            psp34::Internal::_mint_to(self, Self::env().caller(), Id::Bytes(phone_number))
+            psp34::Internal::_mint_to(self, Self::env().caller(), Id::Bytes(phone_number))?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        #[modifiers(only_owner)]
+        pub fn set_metadata(
+            &mut self,
+            phone_number: String,
+            owner_name: String,
+            id: Id,
+        ) -> Result<(), PSP34Error> {
+            let phone_number_key = String::from("phone_number");
+            let owner_name_key = String::from("owner_name");
+            metadata::Internal::_set_attribute(self, id.clone(), phone_number_key, phone_number);
+            metadata::Internal::_set_attribute(self, id, owner_name_key, owner_name);
+            Ok(())
         }
     }
 
