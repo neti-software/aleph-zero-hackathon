2ce806b68b65821d1d27aa1459738d3c77c7bbdb
Filip Lelek <filip.lelek@neti-soft.com>
2024-01-25 15:53:29 +0100

    Homepage only with connect button

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,8 +1,23 @@
 'use client'
 
+// import { useRouter } from 'next/navigation'
+// import { useEffect } from 'react'
+// import { useInkathon } from '@scio-labs/use-inkathon'
 import AppBar from '@/components/ui/app-bar'
 
 export default function Dashboard() {
+  // const router = useRouter()
+
+  // Display `useInkathon` error messages (optional)
+  // const { isConnected } = useInkathon()
+  // useEffect(() => {
+  //   if (!isConnected) {
+  //     router.replace('/')
+  //   } else {
+  //     router.replace('/dashboard')
+  //   }
+  // }, [!isConnected])
+
   return (
     <>
       <AppBar></AppBar>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,37 +1,29 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 import { useInkathon } from '@scio-labs/use-inkathon'
 
-import AppBar from '@/components/ui/app-bar'
-import { ChainInfo } from '@/components/web3/chain-info'
-import { GreeterContractInteractions } from '@/components/web3/greeter-contract-interactions'
+import { ConnectButton } from '@/components/web3/connect-button'
 
 export default function HomePage() {
   const router = useRouter()
 
   // Display `useInkathon` error messages (optional)
-  const { error } = useInkathon()
-  // useEffect(() => {
-  //   if (isLoggedIn) {
-  //     router.replace('/admin/associations')
-  //   } else {
-  //     router.replace('/auth/login')
-  //   }
-  // }, [isLoggedIn])
+  const { connect } = useInkathon()
+  useEffect(() => {
+    if (connect) {
+      router.replace('/dashboard')
+    } else {
+      router.replace('/')
+    }
+  }, [connect])
 
   return (
     <>
-      <AppBar></AppBar>
       <div className="container relative flex grow flex-col items-center justify-center">
-        <div className="mt-12 flex w-full flex-wrap items-start justify-center gap-4">
-          {/* Chain Metadata Information */}
-          <ChainInfo />
-
-          {/* Greeter Read/Write Contract Interactions */}
-          <GreeterContractInteractions />
-        </div>
+        <ConnectButton />
       </div>
     </>
   )
