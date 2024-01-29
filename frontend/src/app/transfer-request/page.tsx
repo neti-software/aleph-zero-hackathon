80480dc76167d769d72149a38d208e776465d2d7
Filip Lelek <filip.lelek@neti-soft.com>
2024-01-29 15:57:22 +0100

    Add pages

---
+++ frontend/src/app/transfer-request/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function TransferRequest() {
+  return (
+    <>
+      <div className="text-whit container relative flex grow flex-col items-center justify-center">
+        transfer request
+      </div>
+    </>
+  )
+}
